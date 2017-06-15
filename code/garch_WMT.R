d9b765d2c09cb3e7c64d1ba46e2414a097ac91a4
xuning28 <ningxu2008@163.com>
2017-06-16 00:15:57 +0800

    我把code合在一起了，latex录入了

---
+++ code/garch_WMT.R
@@ -0,0 +1,26 @@
+## Testing ARCH Effects
+library(TSA)
+library(xts)
+library(xtable)
+library(lubridate)
+library(readxl)
+load("/FDA_project_group_XN/data/WMT_all.RData")
+
+at = lgreturn-mean(lgreturn)
+Box.test(at^2,lag=5,type='Ljung') # perform a Ljung Box test to test ARCH effects
+
+## GARCH Modelling
+library(fGarch)
+
+m1 = garchFit(~ garch(1,0), data = lgreturn, cond.dist = "norm") # Fit an ARCH(1) model
+summary(m1)
+m2 = garchFit(~ garch(1,1), data = lgreturn, cond.dist = "norm") # Fit a GARCH(1,1) model
+summary(m2)
+
+## Prediction
+sigma2=predict(m2,50)
+print(sigma2)
+library(sciplot)
+plot(sigma2$meanForecast,ylim=c(-0.05,0.05))
+lines(c(1:50),sigma2$meanForecast+1.96*sigma2$meanError,col='blue')
+lines(c(1:50),sigma2$meanForecast-1.96*sigma2$meanError,col='blue')
