c2fdf12a55004bbff426c36ddb50b04b63629430
xuning28 <ningxu2008@163.com>
2017-06-18 16:39:43 +0800

    except prediction

--- code/garch_AMZN.R
+++ code/garch_AMZN.R
@@ -4,7 +4,8 @@ library(xts)
 library(xtable)
 library(lubridate)
 library(readxl)
-load("/FDA_project_group_XN/data/AMZN_all.RData")
+AMZN <- load("F:/CUEB/Junior/SPRING/FDA/FDA_project_group_XN/data/AMZN_all.RData",
+                   col_types = c("date", "numeric", "numeric", "numeric", "numeric", "numeric"))
 
 at = lgreturn-mean(lgreturn)
 Box.test(at^2,lag=5,type='Ljung') # perform a Ljung Box test to test ARCH effects
@@ -12,15 +13,45 @@ Box.test(at^2,lag=5,type='Ljung') # perform a Ljung Box test to test ARCH effect
 ## GARCH Modelling
 library(fGarch)
 
-m1 = garchFit(~ garch(1,0), data = lgreturn, cond.dist = "norm") # Fit an ARCH(1) model
+# Normal distributed innovation
+m1 = garchFit(~ garch(1,0), data = lgreturn, cond.dist = "norm") 
+# Fit an ARCH(1) model
 summary(m1)
-m2 = garchFit(~ garch(1,1), data = lgreturn, cond.dist = "norm") # Fit a GARCH(1,1) model
+
+m2 = garchFit(~ garch(1,1), data = lgreturn, cond.dist = "norm") 
+# Fit a GARCH(1,1) model
 summary(m2)
 
+# student-t distributed innovation
+m11 = garchFit(~ garch(1,0), data = lgreturn, cond.dist = "std") 
+# Fit an ARCH(1) model
+summary(m11)
+m22 = garchFit(~ garch(1,1), data = lgreturn, cond.dist = "std") 
+# Fit a GARCH(1,1) model
+summary(m22)
+
+## Time plots of the stock price and trading volume
+plot(col='blue',y=AMZN$close,x=AMZN$date,xlab='year', ylab='price',type='l',main='Stock Price of AMZN 2010-2016')
+plot(col='brown',y=AMZN$volume,x=AMZN$date,xlab='year', ylab='volume',type='l',main='Trading Volume of AMZN 2010-2016')
+
+## Plot the estimated volatility and standardized residuals based on the selected model
+vol_1 <- fBasics::volatility(m2)
+plot(col='purple',y=vol_1,x=AMZN$date,xlab='year', ylab='volatility of AMZN',type='l',main='Volatility of AMZN 2010-2016')
+
+sres_1 <- residuals(m2, standardize=TRUE)
+plot(col='blue',y=sres_1,x=AMZN$date,xlab='year', ylab='Standardized Residuals of AMZN',type='l',main='standardized residuals of AMZN 2010-2016')
+
 ## Prediction
-sigma2=predict(m2,50)
+load("F:/CUEB/Junior/SPRING/FDA/FDA_project_group_XN/data/AMZN2017.RData")
+
+logreturn2017 = diff(log(AMZN2017$close))*100
+print(logreturn2017)
+sigma2=predict(m2,114)
 print(sigma2)
+
 library(sciplot)
-plot(sigma2$meanForecast, ylim=c(-0.05,0.05))
-lines(c(1:50),sigma2$meanForecast+1.96*sigma2$meanError,col='blue')
-lines(c(1:50),sigma2$meanForecast-1.96*sigma2$meanError,col='blue')
+plot(c(1:114),sigma2$meanForecast,ylim=c(-3.0,3.0),type = "l")
+lines(c(1:114),logreturn2017,col='red')
+lines(c(1:114),sigma2$meanForecast+1.96*sigma2$meanError,col='blue')
+lines(c(1:114),sigma2$meanForecast-1.96*sigma2$meanError,col='blue')
+
--- code/garch_WMT.R
+++ code/garch_WMT.R
@@ -4,7 +4,8 @@ library(xts)
 library(xtable)
 library(lubridate)
 library(readxl)
-load("/FDA_project_group_XN/data/WMT_all.RData")
+WMT <- load("F:/CUEB/Junior/SPRING/FDA/FDA_project_group_XN/data/WMT_all.RData",
+                   col_types = c("date", "numeric", "numeric", "numeric", "numeric", "numeric"))
 
 at = lgreturn-mean(lgreturn)
 Box.test(at^2,lag=5,type='Ljung') # perform a Ljung Box test to test ARCH effects
@@ -15,15 +16,44 @@ eacf(lgreturn)
 ## GARCH Modelling
 library(fGarch)
 
-m1 = garchFit(~ garch(4,0), data = lgreturn, cond.dist = "norm") # Fit an ARCH(1) model
+# Normal distributed innovation
+m1 = garchFit(~ garch(4,0), data = lgreturn, cond.dist = "norm") 
+# Fit an ARCH(4) model
 summary(m1)
-m2 = garchFit(~ garch(1,1), data = lgreturn, cond.dist = "norm") # Fit a GARCH(1,1) model
+m2 = garchFit(~ garch(1,1), data = lgreturn, cond.dist = "norm") 
+# Fit a GARCH(1,1) model
 summary(m2)
 
-## Prediction
-sigma2=predict(m2,50)
+# Student-t distributed innovation
+m11=garchFit(~ garch(4,0), data = lgreturn, cond.dist = "std") 
+# Fit an ARCH(4) model
+summary(m11)
+m22 = garchFit(~ garch(1,1), data = lgreturn, cond.dist = "std") 
+# Fit a GARCH(1,1) model 
+summary(m22)
+
+## Time plots of the stock price and trading volume
+plot(col='blue',y=WMT$close,x=WMT$date,xlab='year', ylab='volume',type='l',main='Stock Price of WMT 2010-2016')
+plot(col='brown',y=WMT$volume,x=WMT$date,xlab='year', ylab='volume',type='l',main='Trading Volume of WMT 2010-2016')
+
+## Plot the estimated volatility and standardized residuals based on the selected model
+vol_1 <- fBasics::volatility(m1)
+plot(col='purple',y=vol_1,x=WMT$date,xlab='year', ylab='volatility of WMT',type='l',main='Volatility of WMT 2010-2016')
+
+sres_1 <- residuals(m1, standardize=TRUE)
+plot(col='blue',y=sres_1,x=WMT$date,xlab='year', ylab='standardized residuals of WMT',type='l',main='standardized residuals of WMT 2010-2016')
+
+## Predict
+load("F:/CUEB/Junior/SPRING/FDA/FDA_project_group_XN/data/WMT2017.RData")
+
+logreturn2017 = diff(log(WMT2017$close))*100
+print(logreturn2017)
+sigma2=predict(m1,114)
 print(sigma2)
+
 library(sciplot)
-plot(sigma2$meanForecast,ylim=c(-0.05,0.05))
+plot(c(1:114),sigma2$meanForecast,ylim=c(-3.0,3.0),type = "l")
+lines(c(1:114),logreturn2017,col='red')
 lines(c(1:50),sigma2$meanForecast+1.96*sigma2$meanError,col='blue')
 lines(c(1:50),sigma2$meanForecast-1.96*sigma2$meanError,col='blue')
+
